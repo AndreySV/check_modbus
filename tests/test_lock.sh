d0f95934cb710f03fdf94f837eb1da93e219241c
Andrey Skvortsov <Andrej.Skvortzov@gmail.com>
2014-01-23 12:38:53 +0400

    added license text to all source file and scripts to pass debian's licensecheck.

--- tests/test_lock.sh
+++ tests/test_lock.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+#******************************************************************************
+# check_modbus: checker for Modbus TCP/RTU devices for Nagios control
+# system based on libmodbus library
+
+# Copyright (C) 2011 2012 2013 2014 Andrey Skvortsov
+
+  
+# This program is free software; you can redistribute it and/or modify 
+# it under the terms of the GNU General Public License as published by 
+# the Free Software Foundation; either version 3, or (at your  option)  
+# any later version.
+  
+# This program is distributed in the hope that it will be useful, but  
+# WITHOUT ANY WARRANTY; without even the implied warranty of  
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU    
+# General Public License for more details.
+  
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software  
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301, USA. 
+
+
+# Andrey Skvortsov
+# Andrej . Skvortzov [at] gmail . com
+#******************************************************************************
+
 
 #******************************************************************************
 # run_create_dumpfile
