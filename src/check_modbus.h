9d469f047bebc7024d931892dbe46900da8c5b66
Andrey Skvortsov <Andrej.Skvortzov@gmail.com>
2014-07-31 10:05:35 +0400

    fix trailing whitespaces in headers
    Signed-off-by: Andrey Skvortsov <Andrej.Skvortzov@gmail.com>

--- src/check_modbus.h
+++ src/check_modbus.h
@@ -4,21 +4,21 @@
 
   Copyright (C) 2011 2012 2013 2014 Andrey Skvortsov
 
-  
-  This program is free software; you can redistribute it and/or modify 
-  it under the terms of the GNU General Public License as published by 
-  the Free Software Foundation; either version 3, or (at your  option)  
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3, or (at your  option)
   any later version.
-  
-  This program is distributed in the hope that it will be useful, but  
-  WITHOUT ANY WARRANTY; without even the implied warranty of  
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU    
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU
   General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software  
+  along with this program; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-  02110-1301, USA. 
+  02110-1301, USA.
 
 
   Andrey Skvortsov
@@ -91,13 +91,13 @@ typedef struct
     int     dump_format;                // output format of the dump
     int     dump_size;                  // number of input registers/bits included in the dump
     char*   dump_file;
-    
+
     char*   lock_file_in;               // name of lock file for inputs
     int     lock_file_in_fd;
-    
+
     char*   lock_file_out;              // name of lock file for
     int     lock_file_out_fd;
-    
+
 }   modbus_params_t;
 
 
--- src/global_macro.h
+++ src/global_macro.h
@@ -4,21 +4,21 @@
 
   Copyright (C) 2011 2012 2013 2014 Andrey Skvortsov
 
-  
-  This program is free software; you can redistribute it and/or modify 
-  it under the terms of the GNU General Public License as published by 
-  the Free Software Foundation; either version 3, or (at your  option)  
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3, or (at your  option)
   any later version.
-  
-  This program is distributed in the hope that it will be useful, but  
-  WITHOUT ANY WARRANTY; without even the implied warranty of  
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU    
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU
   General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software  
+  along with this program; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-  02110-1301, USA. 
+  02110-1301, USA.
 
 
   Andrey Skvortsov
