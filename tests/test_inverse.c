c32f3db0d5e58f331c93275643a80eb6310b4bd8
Andrey Skvortsov <Andrej.Skvortzov@gmail.com>
2014-08-03 21:31:32 +0400

    test_inverse.c and test_swap: fix codestyle issues
    Signed-off-by: Andrey Skvortsov <Andrej.Skvortzov@gmail.com>

--- tests/test_inverse.c
+++ tests/test_inverse.c
@@ -4,21 +4,21 @@
 
   Copyright (C) 2011 2012 2013 2014 Andrey Skvortsov
 
-  
-  This program is free software; you can redistribute it and/or modify 
-  it under the terms of the GNU General Public License as published by 
-  the Free Software Foundation; either version 3, or (at your  option)  
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3, or (at your  option)
   any later version.
-  
-  This program is distributed in the hope that it will be useful, but  
-  WITHOUT ANY WARRANTY; without even the implied warranty of  
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU    
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU
   General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software  
+  along with this program; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-  02110-1301, USA. 
+  02110-1301, USA.
 
 
   Andrey Skvortsov
@@ -41,33 +41,31 @@ static int test_inverse(int format)
 
 	before.format = format;
 	after.format  = format;
-	
-	size_words  = sizeof_data_t( &before ); /* sizeof_data_t returs size in words */
-	for(i=0; i<size_words; i++ )
-	{
+
+	/* sizeof_data_t returs size in words */
+	size_words  = sizeof_data_t(&before);
+	for (i = 0; i < size_words; i++) {
 		before.val.words[i] = i;
 		after.val.words[size_words-i-1] = i;
 	}
 
-	reorder_data_t( &before, 0, 1 );
-	// check results
-	return  equal_data_t( &before, &after );
+	reorder_data_t(&before, 0, 1);
+	return  equal_data_t(&before, &after);
 }
 
 
 int main(void)
 {
 	size_t i;
-	int formats[]=
-		{
-			FORMAT_SIGNED_WORD,
-			FORMAT_SIGNED_DWORD,
-			FORMAT_SIGNED_QWORD
-		};
+	int formats[] = {
+		FORMAT_SIGNED_WORD,
+		FORMAT_SIGNED_DWORD,
+		FORMAT_SIGNED_QWORD
+	};
 
-	for(i=0; i<sizeof(formats)/sizeof(formats[0]); i++)
-	{
-		if (!test_inverse( formats[i] )) return -1;
+	for (i = 0; i < sizeof(formats)/sizeof(formats[0]); i++) {
+		if (!test_inverse(formats[i]))
+			return -1;
 	}
 	return 0;
 }
--- tests/test_swap.c
+++ tests/test_swap.c
@@ -4,21 +4,21 @@
 
   Copyright (C) 2011 2012 2013 2014 Andrey Skvortsov
 
-  
-  This program is free software; you can redistribute it and/or modify 
-  it under the terms of the GNU General Public License as published by 
-  the Free Software Foundation; either version 3, or (at your  option)  
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3, or (at your  option)
   any later version.
-  
-  This program is distributed in the hope that it will be useful, but  
-  WITHOUT ANY WARRANTY; without even the implied warranty of  
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU    
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU
   General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software  
+  along with this program; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-  02110-1301, USA. 
+  02110-1301, USA.
 
 
   Andrey Skvortsov
@@ -41,10 +41,10 @@ static int test_swap(int format)
 
 	before.format = format;
 	after.format  = format;
-	
-	size_bytes = 2*sizeof_data_t( &before ); /* sizeof_data_t returns size in words */
-	for(i=0; i<size_bytes; i+=2 )
-	{
+
+	/* sizeof_data_t returns size in words */
+	size_bytes = 2*sizeof_data_t(&before);
+	for (i = 0; i < size_bytes; i += 2) {
 		before.val.bytes[i] = i;
 		before.val.bytes[i+1] = i+1;
 
@@ -53,25 +53,23 @@ static int test_swap(int format)
 		after.val.bytes[i+1] = i;
 	}
 
-	reorder_data_t( &before, 1, 0);
-	// check results
-	return  equal_data_t( &before, &after );
+	reorder_data_t(&before, 1, 0);
+	return  equal_data_t(&before, &after);
 }
 
 
 int main(void)
 {
 	size_t i;
-	int formats[]=
-		{
-			FORMAT_SIGNED_WORD,
-			FORMAT_SIGNED_DWORD,
-			FORMAT_SIGNED_QWORD
-		};
+	int formats[] = {
+		FORMAT_SIGNED_WORD,
+		FORMAT_SIGNED_DWORD,
+		FORMAT_SIGNED_QWORD
+	};
 
-	for(i=0; i<sizeof(formats)/sizeof(formats[0]); i++)
-	{
-		if (!test_swap( formats[i] )) return -1;
+	for (i = 0; i < sizeof(formats)/sizeof(formats[0]); i++) {
+		if (!test_swap(formats[i]))
+			return -1;
 	}
 	return 0;
 }
